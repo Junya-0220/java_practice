947608b107972dba42ec78685a3eb1d5cbd75096
Junya-0220 <junya010220@gmail.com>
2021-03-03 00:06:45 +0900

    commit

---
+++ server/src/Lesson4/Array.java
@@ -0,0 +1,8 @@
+public class Array {
+  public static void main(String[] args) {
+    int[] scores1 = new int[] {20, 30, 40, 50, 80};
+    int[] scores2 = {30, 40, 50, 60, 70};
+    System.out.println(scores1[1]);
+    System.out.println(scores2[1]);
+  }
+}
